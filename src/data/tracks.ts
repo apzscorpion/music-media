6a261c2fbc825b1121d94b7ae785dad0364520ab
asif-mn-aitronos <asif.mn@aitronos.com>
2026-01-15 18:35:14 +0530

    Name update

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -1,20 +1,20 @@
 export const tracks = [
   {
-    id: 'track1',
-    title: 'Vibe thalla (Divya v1)',
-    filename: 'track.enc', // Keeping original filename for first track to avoid breaking changes if user hasn't run script
-    keyId: 'track'
+    id: "track1",
+    title: "Vibe Divya v1",
+    filename: "track.enc", // Keeping original filename for first track to avoid breaking changes if user hasn't run script
+    keyId: "track",
   },
   {
-    id: 'track2',
-    title: 'Broken Basha v1',
-    filename: 'track2.enc',
-    keyId: 'track2'
+    id: "track2",
+    title: "Broken Basha v1",
+    filename: "track2.enc",
+    keyId: "track2",
   },
   {
-    id: 'track3',
-    title: 'Rider Basha v1',
-    filename: 'track3.enc',
-    keyId: 'track3'
-  }
+    id: "track3",
+    title: "Rider Basha v1",
+    filename: "track3.enc",
+    keyId: "track3",
+  },
 ];
